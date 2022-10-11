3b0d5db1c20cf9a7c60f10c4e1aae27bbf6a094d
Ahmed Caferov <ahmetleo56@gmail.com>
2022-10-11 08:33:42 +0400

    add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {},
+    fontFamily: {
+      'CormorantGaramond': ['Cormorant Garamond', 'sans-serif'],
+      'Arimo': ['Arimo', 'sans-serif'],
+    },
   },
   plugins: [],
 }
